037f739372d93fdc6f635e1e6ea6a4d4a8baf557
johndean3326 <113262365+johndean3326@users.noreply.github.com>
2023-04-14 21:10:38 -0600

    git ignore

--- server.js
+++ server.js
@@ -1,26 +1,8 @@
 const db = require("./db/connection.js");
-// const express = require("express");
 const startInquirer = require("./db/queries.js");
 const mysql = require ("mysql2");
-const PORT = process.env.PORT || 3001;
-// const app = express();
 
-// express middleware
-// app.use(express.urlencoded({extended: false}));
-// app.use(express.json());
+require('dotenv').config
 
-// Default response for any other request (Not found)
-// app.use((req, res) => {
-//   res.status(404).end();
-// });
 
-// Start server after DB connection
-db.connect(err => {
-  if (err) throw err;
-  console.log('Database connected.');
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-   
-  });
-});
 startInquirer();
